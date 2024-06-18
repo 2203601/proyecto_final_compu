835a40ad7deec0d7e2f927835fb68bc443008b12
Luciana Hueda <2203601@ucc.edu.ar>
2024-06-18 11:54:35 -0300

    comentamos pingpong

--- pingpong.s
+++ pingpong.s
@@ -3,6 +3,7 @@
 // Arqui I UCC
 
 .text
+// funciones del main 
 
 .global pingpong
 .global SALIR
@@ -12,52 +13,52 @@
 
 pingpong:
         PUSH {R4, R5, R6, R7, R9, R10, R11, LR}  // Guardar registros y el valor de retorno
-       LDR R11, =DELAY_4
-       LDR R6, [R11]
-       LDR R9, = DELAY
-       STR R6, [R9]
-       LDR R10, = SALIR
-        MOV R4, #28           // Establecer el contador de bucle a 12
-        LDR R5, =array_pingpong  // Cargar la dirección base de la secuencia en R5
+       LDR R11, =DELAY_4 // guarda la direccion de delay_4 en R11
+       LDR R6, [R11] // guarda el valor de R11 en R6
+       LDR R9, = DELAY // guarda la direccion de delay en R9
+       STR R6, [R9] // guarda el valor de DELAY_4(R6) en DELAY (R9)
+       LDR R10, = SALIR // guarda la diccion de salir en R10
+        MOV R4, #28           // Establecer el contador de bucle a 28
+        LDR R5, =array_pingpong  // Cargar la dirección del array
         
 loop:   LDRB R6, [R5], #1     // Cargar un byte de la secuencia en R6 y avanzar el puntero
-		LDR R7, [R10]
-        CMP R7, #1
-        BEQ break
+		LDR R7, [R10]   // guarda el valor de R10 en R7
+        CMP R7, #1 // compara  el valor de R7 con 1 para poder salir
+        BEQ break // si SALIR es 1, se termina la funcion
         
         MOV R0, R6             // Mover el valor a R0 para pasar a disp_binary
         BL disp_binary         // Llamar a la función disp_binary
         MVN R0, R6             // Invertir los bits para que los LED se activen con nivel bajo
         BL leds                // Llamar a la función leds
-		LDR R6, [R9]
-        MOV R0, R6          // Establecer el retraso en 400 ms
-        BL Delay         // Llamar a la función delayMillis
+		LDR R6, [R9]            // guarda el valor de R9 en R6 (delay)
+        MOV R0, R6          // Establecer el retraso según R6
+        BL Delay         // Llamar a la función delay
         SUBS R4, R4, #1        // Decrementar el contador de bucle
         BNE loop               // Si no ha llegado a cero, repetir el bucle
         
-        MOV R4, #8
-        LDR R5, =array_end // load base address of array into R5
+        MOV R4, #8          //establece el contador a 8
+        LDR R5, =array_end // guarda el array en R5
         
-loop2:   LDRB R6, [R5], #1
-		LDR R7, [R10]
-        CMP R7, #1
-        BEQ break
+loop2:   LDRB R6, [R5], #1 // Cargar un byte de la secuencia en R6 y avanzar el puntero
+		LDR R7, [R10] // guarda el valor de R10 en R7
+        CMP R7, #1 // compara  el valor de R7 con 1 para poder salir
+        BEQ break // si SALIR es 1, se termina la funcion
         
-        MOV R0, R6
-        BL disp_binary 
+        MOV R0, R6 // Mover el valor a R0 para pasar a disp_binary
+        BL disp_binary // Llamar a la función disp_binary
         MVN R0, R6 // interface outputs are active by low
-        BL leds
-		LDR R6, [R9]
-        MOV R0, R6
-        BL Delay
-        SUBS R4, R4, #1
-        BNE loop2 
+        BL leds // Llamar a la función leds
+		LDR R6, [R9]  // guarda el valor de R9 en R6 (delay)
+        MOV R0, R6 // Establecer el retraso según R6
+        BL Delay  // Llamar a la función delay
+        SUBS R4, R4, #1 // Decrementar el contador de bucle
+        BNE loop2   // Si no ha llegado a cero, repetir el bucle
         
 break:
-		STR R6, [R11]
-		MOV R0, #0
-		STR R0, [R10]
-		POP {R4, R5, R6, R7, R9, R10, R11, PC} 
+		STR R6, [R11] //guarda el valor del delay en delay_4 
+		MOV R0, #0 // establece R0 a 0
+		STR R0, [R10] // establece SALIR a 0
+		POP {R4, R5, R6, R7, R9, R10, R11, PC} // restaura los registros y devuelve
 .data
 
 array_pingpong:
